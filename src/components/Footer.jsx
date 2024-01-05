f606566ca88b43553b6cfc40387790b3241d9944
Bonfanti Cecilia <bonfanticeciliaemilce@gmail.com>
2024-01-05 01:00:40 -0300

    rama footer para componente footer.jsx, se crea y estiliza incluyendo el responsive

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,47 @@
+import { Link } from "react-router-dom";
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import "../styles/footer.css";
 export function Footer() {
-    return (
-      <>
-        <h1>Footer</h1>
-      </>
-    );
-  }
+  return (
+    <>
+      <footer className="fw-100  mt-5 fixed-bottom color-footer py-5">
+        <section className="container py-4">
+          <div className="m-0 p-0 d-flex align-items-center justify-content-between w-100">
+            <article className=" d-flex flex-column align-items-center align-items-md-start gap-2">
+              <Link to={"/about"} className="Link">
+                SOBRE NOSOTROS
+              </Link>
+              <Link to={"/faq"} className="Link">
+                PREGUNTAS FRECUENTES
+              </Link>
+              <div className="d-flex align-items-center gap-4 mt-3 responsive-red">
+                <a href="http://" target="_blank" rel="noopener noreferrer">
+                  <i className="bi bi-facebook fs-2" />
+                </a>
+                <a href="http://" target="_blank" rel="noopener noreferrer">
+                  <i className="bi bi-instagram fs-2" />
+                </a>
+                <a href="http://" target="_blank" rel="noopener noreferrer">
+                  <i className="bi bi-twitter fs-2"></i>
+                </a>
+              </div>
+            </article>
+            
+            <article className="d-flex align-items-center align-items-md-end justify-content-center gap-2">
+              <Link
+                to={"/"}
+                className=" d-flex align-items-center justify-content-center img-footer"
+              >
+                <img
+                  src="/img/branding/isotype.svg"
+                  alt=""
+                  className="img-fluid w-50"
+                />
+              </Link>
+            </article>
+          </div>
+        </section>
+      </footer>
+    </>
+  );
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import { MyNavbar } from "../components/Navbar";
+import { Footer } from "../components/Footer";
 export function Home() {
     return (
       <>
        <MyNavbar />
         <h1>Home</h1>
+        <Footer />
       </>
     );
   }
